072c33d5de1dbe2aefc125b2c912936b65999b5d
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-09 12:51:33 +0200

    Modify App.js to show medicines from list.
    * Modify test to pass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from "react";
+const medicineItems = [
+  {
+    id: 1,
+    name: "Gripex",
+    description: "Best ever",
+    med_form: "Ointment",
+    med_type: "Antybiotic"
+  },
+  {
+    id: 2,
+    name: "Apap",
+    description: "Painkiller",
+    med_form: "Ointment",
+    med_type: "Antybiotic"
+  },
+  {
+    id: 3,
+    name: "ClearSKy",
+    description: "For eye",
+    med_form: "Ointment",
+    med_type: "Antybiotic"
+  },
+  {
+    id: 4,
+    name: "WindWear",
+    description: "Best probiotic for rain",
+    med_form: "Ointment",
+    med_type: "Probiotic"
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      medicineList: medicineItems
+    };
+  }
+  renderItems = () => {
+    const newItems = this.state.medicineList;
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={"medicine-name mr-2"}
+          title={item.name}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {item.id} - {item.name}
+        </span>
+        <span>
+          <button className="btn btn-secondary mr-2"> Edit </button>
+          <button className="btn btn-danger">Delete </button>
+        </span>
+      </li>
+    ));
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Med-files app</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add medicine</button>
+              </div>
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
-
 export default App;
